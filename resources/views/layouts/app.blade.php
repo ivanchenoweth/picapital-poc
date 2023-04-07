d9ed8511a6900c72a879aa8b91a01169442947a5
Ivan R. Chenoweth <ivanchenoweth@gmail.com>
2023-04-07 19:58:00 +0000

    change footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,16 +15,7 @@
       <div class="row">
          <div class="col-md-6">
             <h3>Contacto</h3>
-            <p>Correo electrónico: contacto@tudominio.com</p>
-            <p>Teléfono: 1234567890</p>
-         </div>
-         <div class="col-md-6">
-            <h3>Síguenos en redes sociales</h3>
-            <ul class="social-media">
-               <li><a href="#"><i class="fab fa-facebook"></i></a></li>
-               <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-               <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-            </ul>
+            <p>Correo electrónico: contacto@picapital.mx</p>
          </div>
       </div>
    </div>
