50139b7e7d6bae52a726f0cb87ba87f45211d02c
Ivan R. Chenoweth <ivanchenoweth@gmail.com>
2023-04-08 05:06:46 +0000

    Css new

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel</title>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="css/app.css">
     </head>
 
     
